221c3dcb49c32ce5fe5c8f704f23a970d597238b
katherinegemperline <kategemperline@gmail.com>
2019-04-02 21:14:44 -0400

    Still working on the button, maybe half-way there?

--- csci260/project/ui/flower-power-UI/src/app/app.module.ts
+++ csci260/project/ui/flower-power-UI/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -13,6 +13,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
