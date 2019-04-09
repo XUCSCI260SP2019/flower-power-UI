2734ab82353868c9cf4366a07dfe3ffa6a370a83
katherinegemperline <kategemperline@gmail.com>
2019-04-09 09:15:25 -0400

    Creating login; must be logged in before accessing my-professors-page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {UserServiceService} from '../user-service.service';
+// http://gnomeontherun.com/2017/03/02/guards-and-login-redirects-in-angular/
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  username: '';
+  password: '';
+  return: '';
 
-  constructor() { }
+  constructor(
+    private userService: UserServiceService,
+    private router: Router,
+    private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // Get the query params
+    this.route.queryParams
+      .subscribe(params => this.return = params['return'] || '/forums');
   }
 
+  login() {
+    /*
+    if (this.username && this.password) {
+      this.userService.login(this.username);
+      this.router.navigateByUrl(this.return);
+    */
+    if (this.username === '' && this.password === '' ) {
+      this.router.navigateByUrl(this.return);
+    }
+  }
 }
