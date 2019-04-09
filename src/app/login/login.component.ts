27969245772abf86f397d9ade1b109f5e6709c2d
katherinegemperline <kategemperline@gmail.com>
2019-04-09 09:39:05 -0400

    form for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,15 @@
-import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import {UserServiceService} from '../user-service.service';
-// http://gnomeontherun.com/2017/03/02/guards-and-login-redirects-in-angular/
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  username: '';
-  password: '';
-  return: '';
+export class LoginComponent implements OnInit {
 
-  constructor(
-    private userService: UserServiceService,
-    private router: Router,
-    private route: ActivatedRoute) {}
+  constructor() { }
 
   ngOnInit() {
-    // Get the query params
-    this.route.queryParams
-      .subscribe(params => this.return = params['return'] || '/forums');
   }
 
-  login() {
-    /*
-    if (this.username && this.password) {
-      this.userService.login(this.username);
-      this.router.navigateByUrl(this.return);
-    */
-    if (this.username === '' && this.password === '' ) {
-      this.router.navigateByUrl(this.return);
-    }
-  }
 }
