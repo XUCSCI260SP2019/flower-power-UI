b75a0e84fb0b71d661a042d339ce372f738ad212
katherinegemperline <kategemperline@gmail.com>
2019-04-23 11:21:46 -0400

    created a returned user class

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+  ) {}
   doLogin(user: User): void { }
 
   // look at hero.service to post user to url for back-end services
