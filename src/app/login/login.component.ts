374ef941ba106336e357f1675d4c886b5d04b397
katherinegemperline <kategemperline@gmail.com>
2019-04-23 10:49:23 -0400

    created a user class to send to back-end services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserServiceService} from '../user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userServiceService: UserServiceService) { } // create login function: to send to user.service
+
+  login(username: string, password: string): void {
+    this.userServiceService.doLogin({username, password} as User);
+  }
 
   ngOnInit() {
   }
--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +7,7 @@ import { Injectable } from '@angular/core';
 export class UserServiceService {
 
   constructor() {}
+  doLogin(user: User): void { }
 
-  getInfo() {
-    return true;
-  }
+  // look at hero.service to post user to url for back-end services
 }
