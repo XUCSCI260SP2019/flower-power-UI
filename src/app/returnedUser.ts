0eef83e2cedc79ac3eb2b2d8bb637c8f0423ee11
katherinegemperline <kategemperline@gmail.com>
2019-04-25 08:41:21 -0400

    reroute

--- src/app/returnedUser.ts
+++ src/app/returnedUser.ts
@@ -1,11 +1,9 @@
 export class User {
   username: string;
-  password: string;
   FirstName: string;
   LastName: string;
   admin: boolean;
   email: string;
-  userType: string
   loggedIn: boolean;
   // store the information we receive from login
 }
