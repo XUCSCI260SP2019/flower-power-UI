0520846e76fe48dc775d1a993dcb87ac5ebc9449
katherinegemperline <kategemperline@gmail.com>
2019-04-30 09:37:00 -0400

    fixed a few errors

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewUserService} from '../new-user.service';
 import { NewUser} from '../nUser';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-new-user',
@@ -10,11 +11,17 @@ import { NewUser} from '../nUser';
 export class NewUserComponent implements OnInit {
 
   constructor(
-    private newUserService: NewUserService
+    private newUserService: NewUserService,
+    private location: Location
   ) { }
 
   create(first: string, last: string, uname: string, psw: string, email: string, phone: string, professor: boolean): void {
     this.newUserService.createUser({first, last, uname, psw, email, phone, professor} as NewUser).subscribe(() => this.goBack());
+  }
+
+  // this might no be working? it's not currently called anywhere, so call it after going login?
+  goBack(): void {
+    this.location.go('/home');
   }
 
   ngOnInit() {
