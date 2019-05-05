1698ea3fbd4f511f8a036ff9a4e7c6db5c4bf5a2
katherinegemperline <kategemperline@gmail.com>
2019-05-05 15:26:45 -0400

    fixed dropdowns

--- src/app/reserve-time/reserve-time.component.ts
+++ src/app/reserve-time/reserve-time.component.ts
@@ -8,6 +8,13 @@ import { Request } from '../request';
   styleUrls: ['./reserve-time.component.css']
 })
 export class ReserveTimeComponent implements OnInit {
+  name: string;
+  startminutes: string;
+  starthours: string;
+  starttime: string;
+  endminutes: string;
+  endhours: string;
+  endtime: string;
 
   constructor(
     private reservationsService: ReservationsService,
@@ -16,9 +23,36 @@ export class ReserveTimeComponent implements OnInit {
   ngOnInit() {
   }
 
-  reserveTime(starttime: string, endtime: string, fullname: string): void {
+  reserveTime(): void {
+    this.starttime = this.starthours + this.startminutes;
+    this.endtime = this.endhours + this.endminutes;
     console.log('this.reserveTime is called');
-    this.reservationsService.requestTime(starttime, endtime, fullname);
+    this.reservationsService.requestTime(this.starttime, this.endtime, this.name);
+  }
+
+  studentName(name: string): void {
+    console.log('student name is called');
+    this.name = name;
+  }
+
+  startTimeMinute(mins: string): void {
+    console.log('start minutes is recorded?');
+    this.startminutes = mins;
+  }
+
+  startTimeHour(hrs: string): void {
+    console.log('start hour is recorded');
+    this.starthours = hrs;
+  }
+
+  endTimeMinute(emins: string): void {
+    console.log('end time is reorded');
+    this.endminutes = emins;
+  }
+
+  endTimeHour(ehrs: string): void {
+    console.log('end hour is recorded');
+    this.endhours = ehrs;
   }
 }
 
